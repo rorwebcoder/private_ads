2e48196f56c1c0120d08a539744423a921cdea71
rorwebcoder <ror.webcoder@gmail.com>
2012-09-23 16:52:30 +0530

    Rails application created & pushed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrivateAds::Application.config.secret_token = 'b55c4c4d16d1e045e99c1352f0db8e8f0fb59094f8d60b50ec1b6c64df10c704e689184765549c810e0c7efa84dad01ac6fc928ea66b5bb9eb25b2f9e2ac875e'
